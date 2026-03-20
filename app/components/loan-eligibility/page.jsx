1cd19ae9ef46c7ac802163627661a293c9745757
pritesh <pritesh@kali.pritesh>
2026-03-20 17:25:32 +0530

    kyc updated

--- app/components/loan-eligibility/page.jsx
+++ app/components/loan-eligibility/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getWithAuth, postWithAuth } from "../lib/api";
+import { getWithAuth, postWithAuth } from "../../lib/api";
 import {
   TrendingUpIcon,
   TrendingDownIcon,
